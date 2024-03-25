8e50f9bd812af0142167da370757080c5bacbfb6
Nithin <nithinsgayathri@gmail.com>
2024-03-25 21:08:23 +0530

    Remove CORS

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,20 +1,20 @@
-const whitelist = [
-  'http://localhost:5000',
-  'http://localhost:5173',
-  'http://localhost',
-  'https://chuck-norris-backend.vercel.app/',
-  'https://chuck-norris-backend-nithin-sivakumars-projects.vercel.app/',
-  'https://chuck-norris-backend-git-main-nithin-sivakumars-projects.vercel.app/'
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    // console.log('Origin:', origin);
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
+// const whitelist = [
+//   'http://localhost:5000',
+//   'http://localhost:5173',
+//   'http://localhost',
+//   'https://chuck-norris-backend.vercel.app/',
+//   'https://chuck-norris-backend-nithin-sivakumars-projects.vercel.app/',
+//   'https://chuck-norris-backend-git-main-nithin-sivakumars-projects.vercel.app/'
+// ];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // console.log('Origin:', origin);
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// };
 
-export { corsOptions };
+// export { corsOptions };
