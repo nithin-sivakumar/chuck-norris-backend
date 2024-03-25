e6b05357349a18f85539f784a1fe43a9e3614720
Nithin <nithinsgayathri@gmail.com>
2024-03-25 21:29:56 +0530

    CORS allow anywhere

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const app = express();
 import indexRouter from './routes/index.route.js';
-// import cors from 'cors';
-// import { corsOptions } from './config/cors.config.js';
+import cors from 'cors';
+import { corsOptions } from './config/cors.config.js';
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
