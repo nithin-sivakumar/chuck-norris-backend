77ccb759357308513bab28059de5429eabf0b734
Nithin <nithinsgayathri@gmail.com>
2024-03-25 21:06:59 +0530

    CORS updation

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,7 +1,10 @@
 const whitelist = [
   'http://localhost:5000',
   'http://localhost:5173',
-  'http://localhost'
+  'http://localhost',
+  'https://chuck-norris-backend.vercel.app/',
+  'https://chuck-norris-backend-nithin-sivakumars-projects.vercel.app/',
+  'https://chuck-norris-backend-git-main-nithin-sivakumars-projects.vercel.app/'
 ];
 const corsOptions = {
   origin: function (origin, callback) {
